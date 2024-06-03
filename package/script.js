72a8389c978e5c27c4d5a34da43f0e672efa4d6f
Patrik Martinko <patrik.martinko@gmail.com>
2024-06-03 18:54:46 +0200

    v1.1.0 Bard -> Gemini

--- package/script.js
+++ package/script.js
@@ -1,13 +1,14 @@
 let query = (new URLSearchParams(location.search)).get('q');
 if (query) {
 	query = decodeURIComponent(query);
-	const interval = setInterval(() => {
-		const input = document.querySelector('#mat-input-0');
+	let interval = setInterval(() => {
+		const input = document.querySelector('rich-textarea p');
 		if (input) {
 			clearInterval(interval);
-			input.value = query;
-			input.dispatchEvent(new Event('input'));
-			document.querySelector('button.send-button').click();
+			input.textContent = query;
+			setTimeout(() => {
+				document.querySelector('button[aria-label="Send message"]').click();
+			}, 1000);
 		}
 	}, 500);
 }
